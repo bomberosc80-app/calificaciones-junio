1abf8d584719d64ae284c8b18d7889bfea877257
bomberosc80-app <sergionovo88@gmail.com>
2025-07-03 00:28:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function renderCalificaciones(mesSeleccionado) {
   <div class="firma-digital-horizontal fade-in">
     <hr class="linea-firma">
     <span class="icono-firma">🖊️</span>
-    Firmado digitalmente por Epelde Edgardo, Jefe de cuerpo activo.
+    Firmado digitalmente por:
     <br>
     <img src="https://bomberosc80-app.github.io/calificaciones-junio/firma.png" alt="Firma Jefe" class="imagen-firma" draggable="false">
   </div>
